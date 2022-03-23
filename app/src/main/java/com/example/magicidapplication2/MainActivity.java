c95fe5e90f76e6355453522403d60290533ef953
Eshel Shemi <eshel2017@gmail.com>
2022-03-23 22:09:00 +0200

    New Java file

--- app/src/main/java/com/example/magicidapplication2/MainActivity.java
+++ app/src/main/java/com/example/magicidapplication2/MainActivity.java
@@ -1,14 +1,25 @@
-package com.example.magicidapplication2;
+      package com.example.magicidapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+      public class MainActivity extends AppCompatActivity {
+
+EditText etID;
+Button btnSubmit;
+TextView tvResult;
 
       @Override
       protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.main_activity);
+
+      etID = findViewById(R.id.etID);
+      btnSubmit = findViewById(R.id.btnSubmit);
+      tvResult = findViewById(R.id.tvResults);
       }
 }
