13897c77bd2b60a7dcdd1eb7e2209734e517a460
Eshel Shemi <eshel2017@gmail.com>
2022-03-24 20:44:25 +0200

    New Number to press

--- app/src/main/java/com/example/magicidapplication2/MainActivity.java
+++ app/src/main/java/com/example/magicidapplication2/MainActivity.java
@@ -3,6 +3,7 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,5 +22,40 @@ TextView tvResult;
       etID = findViewById(R.id.etID);
       btnSubmit = findViewById(R.id.btnSubmit);
       tvResult = findViewById(R.id.tvResults);
+
+      btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+         String idNumber = etID.getText().toString().trim();
+
+         String dob = idNumber.substring(0 , 6);
+
+         int gender = Integer.parseInt(Character.toString(idNumber.charAt(6)));
+
+         String sGender;
+
+         if (gender < 5 )
+               sGender = getString(R.string.female);
+          else sGender=getString(R.string.male);
+
+          int nationality = Integer.parseInt(Character.toString(idNumber.charAt(10)));
+
+          String sNationality;
+          if (nationality == 0)
+                sNationality = getString(R.string.sacit);
+          else
+                sNationality = getString(R.string.permanent);
+
+String text = getString(R.string.dob) + dob + getString(R.string.newline)
+        + "Gender : " + sGender + "\n" + getString(R.string.nationality) + sNationality;
+
+          tvResult.setText(text);
+
+         }
+
+      });
+
+
       }
 }
